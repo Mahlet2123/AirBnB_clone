d07ef492b39fb29399531db28b4b53ddcca2c959
surafel27 <surafelfekadu399@gmail.com>
2023-02-11 09:04:07 -0800

    fixing base models

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """class BaseModel"""
 import uuid
-from datetime import datetime
+import datetime
 from models import storage
 
 
@@ -16,25 +16,28 @@ class BaseModel:
                 if key == "__class__":
                     pass
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value, date_format)
+                    self.created_at = datetime.datetime. \
+                                      strptime(value, date_format)
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, date_format)
+                    self.updated_at = datetime.datetime. \
+                                      strptime(value, date_format)
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = str(datetime.now())
-            self.updated_at = str(datetime.now())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
             storage.new(self)
 
     def __str__(self):
         """str method"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}" \
+               .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime"""
-        self.updated_at = str(datetime.now())
+        self.updated_at = datetime.datetime.now()
         storage.save()
 
     def to_dict(self):
