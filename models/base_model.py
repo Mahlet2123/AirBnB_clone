89d8f2d6c1f29cb7c81a95f311a2231e3a739d61
mahlet <akotet2123@gmail.com>
2023-02-12 13:07:44 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,60 @@
 #!/usr/bin/python3
-""" Class BaseModel """
-from datetime import datetime
-from uuid import uuid4
+"""class BaseModel"""
+
 import models
+import uuid
+from datetime import datetime
+
+date = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """ construct """
+    """defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """ Construct """
-        if kwargs:
+        """the constructor of a BaseModel"""
+        if kwargs and kwargs is not None:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == 'created_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if 'id' not in kwargs.keys():
-                    self.id = str(uuid4())
-                if 'created_at' not in kwargs.keys():
-                    self.created_at = datetime.now()
-                if 'updated_at' not in kwargs.keys():
-                    self.updated_at = datetime.now()
-                setattr(self, key, value)
+                if key == "__class__":
+                    continue    
+                elif key == "created_at":
+                    self.created_at = datetime. \
+                                      strptime(value, date)
+                elif key == "updated_at":
+                    self.updated_at = datetime. \
+                                      strptime(value, date)
+                else:
+                    if 'id' not in kwargs.keys():
+                        self.id = str(uuid4())
+                    if 'created_at' not in kwargs.keys():
+                        self.created_at = datetime.now()
+                    if 'updated_at' not in kwargs.keys():
+                        self.updated_at = datetime.now()
+                    setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """ String """
-        return('[' + type(self).__name__ + '] (' + str(self.id) +
-               ') ' + str(self.__dict__))
+        """str method"""
+        return "[{}] ({}) {}" \
+               .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """ save function """
-        self.updated_at = datetime.now()
+        """updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """ Return a dictonary """
-        aux_dict = self.__dict__.copy()
-        aux_dict['__class__'] = self.__class__.__name__
-        aux_dict['created_at'] = self.created_at.isoformat()
-        aux_dict['updated_at'] = self.updated_at.isoformat()
-        return aux_dict
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        if "created_at" in new_dict:
+            new_dict["created_at"] = self.created_at.isoformat()
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
