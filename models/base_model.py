97c648942b9120e3e0bb00f9b5811eb26e6059b2
mahlet <akotet2123@gmail.com>
2023-02-12 23:41:03 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -21,14 +21,14 @@ class BaseModel:
                     self.created_at = datetime.strptime(value, date)
                 elif key == "updated_at":
                     self.updated_at = datetime.strptime(value, date)
-                else:
-                    if "id" not in kwargs.keys():
-                        self.id = str(uuid4())
-                    if "created_at" not in kwargs.keys():
-                        self.created_at = datetime.now()
-                    if "updated_at" not in kwargs.keys():
-                        self.updated_at = datetime.now()
-                    setattr(self, key, value)
+
+                if "id" not in kwargs.keys():
+                    self.id = str(uuid4())
+                if "created_at" not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if "updated_at" not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
