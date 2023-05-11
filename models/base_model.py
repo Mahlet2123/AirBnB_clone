f30d753f903ed8d717180a935422469c50c292c2
mahlet <akotet2123@gmail.com>
2023-05-11 11:01:38 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ BaseModel Module """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel():
@@ -21,8 +21,8 @@ class BaseModel():
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ string representation of objects """
@@ -32,11 +32,13 @@ class BaseModel():
     def save(self):
         """ updates the public instance attribute updated_at
         with the current datetime """
-        updated_at = datetime.datetime.now()
+        updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values
         of __dict__ of the instance """
         #setattr(self.__dict__, "__class__", self.__class__.__name__)
         self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
         return self.__dict__
