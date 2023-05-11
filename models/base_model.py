e74c589fd096e3a143671d0cfee9a37786288936
mahlet <akotet2123@gmail.com>
2023-05-11 11:03:58 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel():
     def __str__(self):
         """ string representation of objects """
         #should print: [<class name>] (<self.id>) <self.__dict__>
-        return "[{}]({}){}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ updates the public instance attribute updated_at
